2e89794548160ea932bbd940a96a9cd945eaa4e4
Federico Combi <fedecombi2000@gmail.com>
2026-06-05 11:40:13 +0200

    creazione struttura

--- main.py
+++ main.py
@@ -1,17 +1,23 @@
 '''
 carica dataset
+
 EDA
 - individuazione outlaier
 - indiduazione caratteri strani o nulli
-- guarda grafici corelazione, simmetria
+- guarda grafici correlazione, simmetria (ogni attribuit)
+- test normalità (funzione mat per vedere regolarita')
+
 cleaning
 - elimina duplicati
 - elimina outlier
 - sostituisci caratteri strani
 - trasforma target in 0 1: dove 0 edibile 1 non
+
 logistic regression per machine learning
 implementa la random forset classification
 - scrivi anche un breve spiegazione
+
 flask
 docker
 '''
+
