f07eee7b6b9e864d9e45538f0c7550a1822f5d80
Federico Combi <fedecombi2000@gmail.com>
2026-06-05 11:33:25 +0200

    create repository

---
+++ main.py
@@ -0,0 +1,17 @@
+'''
+carica dataset
+EDA
+- individuazione outlaier
+- indiduazione caratteri strani o nulli
+- guarda grafici corelazione, simmetria
+cleaning
+- elimina duplicati
+- elimina outlier
+- sostituisci caratteri strani
+- trasforma target in 0 1: dove 0 edibile 1 non
+logistic regression per machine learning
+implementa la random forset classification
+- scrivi anche un breve spiegazione
+flask
+docker
+'''
